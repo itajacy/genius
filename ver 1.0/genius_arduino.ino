e5a127eb0cfa5f35c5d068e8671d4686bd7674bc
itajacy <76431908+itajacy@users.noreply.github.com>
2023-10-13 08:56:12 -0300

    ALTERADO O ARQUIVO genius_arduino.ino

--- ver 1.0/genius_arduino.ino
+++ ver 1.0/genius_arduino.ino
@@ -46,6 +46,7 @@ void loop() {
 /************************* Funções **************************/
 
 void proximaRodada() {
+   /********ADICIONADA SOMENTE PARA ESTUDOS******/
   randomSeed(analogRead(A0));
   int sorteio = random(4);
   sequencia[rodada] = sorteio;
